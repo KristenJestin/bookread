9e3a5c3b147f758b11cd4fd6241a655b9fe62f47
Kristen <kris.jestin@laposte.net>
2020-11-21 16:43:28 +0100

    config: add env config management

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,9 @@
-module.exports = {
-	presets: ['module:metro-react-native-babel-preset'],
+module.exports = (api) => {
+	api.cache(true)
+	return {
+		presets: [
+			'module:metro-react-native-babel-preset',
+			'module:react-native-dotenv',
+		],
+	}
 }
